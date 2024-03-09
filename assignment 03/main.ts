8bd9352789c459ae20ebab0c2bab3b4b52db60f7
Abdullah <abdullahboy241@gmail.com>
2024-03-10 04:34:21 +0500

    first commit

---
+++ assignment 03/main.ts
@@ -0,0 +1,9 @@
+// storing the famous quote and its author
+
+let quote:string = "The only way to do great work is to love what you do"
+
+let author:string = "Abdullah Nasir"
+
+//printing the message
+
+console.log(`"${quote}" - ${author}`);
